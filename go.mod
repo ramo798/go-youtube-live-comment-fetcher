9e2b3f335121f66eaf177ec8d85b4843149edb77
ramo798 <knht31i@gmail.com>
2021-12-08 01:35:16 +0900

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ramo798/go-youtube-live-comment-fetcher
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+}
+
+// https://qiita.com/ara1yu81/items/47ebb2272f6a148095d6#%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%88%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B
